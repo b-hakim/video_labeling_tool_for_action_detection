c128664ccbb075efe6ef79557e0631e56d35d00c
bassel <kasparov092@gmail.com>
2018-11-07 05:17:28 +0200

    extra script

--- rotate_video.py
+++ rotate_video.py
@@ -1,3 +1,5 @@
+import glob
+
 import numpy
 
 import cv2
@@ -40,6 +42,7 @@ def rotate_video(list_video_path):
             except:
                 print("error occurred at path " + vid_path)
 
+
 def rem_audio_video(list_video_path, out_dir, front_side, initialize_counter=0):
     with open(list_video_path) as fw:
         c = initialize_counter
@@ -77,8 +80,52 @@ def rem_audio_video(list_video_path, out_dir, front_side, initialize_counter=0):
                 print("error occurred at path " + vid_path)
             # os.rename(vid_path2, vid_path)
 
+
+def rename_files(dir, start, end, new_start):
+    new_name_start = new_start
+
+    for i in range(start, end+1):
+        file_name = os.path.join(dir, "s_{:03}.mp4".format(i))
+        file_new_name = os.path.join(dir, "s_{:03}.mp4".format(new_name_start))
+        new_name_start += 1
+
+        os.rename(file_name, file_new_name)
+
+
+def order_files(dir, char):
+    files = glob.glob(dir+"*.mp4")
+
+    for file_name in files:
+        file_new_name = file_name.replace(".", "_2.")
+        os.rename(file_name, file_new_name)
+
+    files = glob.glob(dir+"*.mp4")
+    files.sort()
+    counter = 0
+
+    for file_name in files:
+        file_new_name = dir+char+"_{:03}".format(counter)
+        counter += 1
+        os.rename(file_name, file_new_name)
+
+
+def add_mp4_extension(dir):
+    files = glob.glob(dir+"/*")
+
+    for file_name in files:
+        file_new_name = file_name+".mp4"
+        os.rename(file_name, file_new_name)
+
+
+
 if __name__ == '__main__':
     # rotate_video("/home/bassel/data/office-actions/to_rotate")
-
+    # rotate_video("/home/bassel/data/office-actions/tt")
     # rem_audio_video("/home/bassel/data/office-actions/rem_audio", "/home/bassel/data/office-actions/office_actions_19/side_view/", 's', 42)
-    rem_audio_video("/home/bassel/data/office-actions/rem_audio2", "/home/bassel/data/office-actions/office_actions_19/front_view/vds/", 'f')
+    # rem_audio_video("/home/bassel/data/office-actions/rem_audio2", "/home/bassel/data/office-actions/office_actions_19/front_view/vds/", 'f')
+    # rename_files("/home/bassel/data/office-actions/office_actions_19/side_view/",
+    #              24, 41, 80)
+    # order_files("/home/bassel/data/office-actions/office_actions_19/front_view/", "f")
+    # order_files("/home/bassel/data/office-actions/office_actions_19/side_view/", "s")
+    # add_mp4_extension("/home/bassel/data/office-actions/office_actions_19/side_view/")
+    pass
